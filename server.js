8e4026471ae357c9f8765bf2148807b1b62e6d72
Yllen Fernandez <fer19020@byui.edu>
2023-01-08 17:33:46 +0100

    changes on json package

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+app.use('/', require('./src/routes'))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
