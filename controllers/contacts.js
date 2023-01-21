963268232f57f2550ea49b5d144e97eded0f31a6
Yllen Fernandez <fer19020@byui.edu>
2023-01-21 23:57:57 +0100

    Personal03 completed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -24,4 +24,64 @@ const getSingleCont  = async (req, res, next) =>{
     })
 }
 
-module.exports = {getCont, getSingleCont}
+//Creating POST that creates new contact that returns the new ID of contact
+const createCont = async (req, res, next) =>{
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+      };
+      const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+      if (response.acknowledged) {
+        res.status(201).json(response);
+      } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+      }
+   
+}
+
+//Create a PUT route to update a contact. This route should allow for a url similar to 
+//this: api-url-path/contacts/id-to-modify. (The id won't be modified, it will just be the
+// means of finding a specific document in the database.) Return an http status code representing
+// the successful completion of the request.
+
+const updateCont = async (req, res, next) =>{
+    const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+}
+
+//Create a DELETE route to delete a contact. Return an http status code representing the 
+//successful completion of the request.
+
+const deleteCont = async (req, res, next) =>{
+    const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+}
+
+module.exports = {getCont, getSingleCont, createCont, updateCont, deleteCont}
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', contactsController.getCont);
 
 router.get('/:id', contactsController.getSingleCont);
 
+router.delete('/:id', contactsController.deleteCont);
+
+router.put('/:id', contactsController.updateCont);
+
+router.post('/', contactsController.createCont);
+
 module.exports = router;
